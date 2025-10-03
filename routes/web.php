b154d898b17093739721e362ab37b7bf13bfc264
induranga bandara <neo.induranga@gmail.com>
2025-10-04 01:54:28 +0530

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::view('/dashboard', 'videos.upload')->name('videos.upload')->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/', 'videos.upload')->name('videos.upload')->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::view('/videos/upload', 'videos.upload')->name('videos.upload');
+require __DIR__.'/auth.php';
